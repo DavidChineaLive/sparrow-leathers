baed390c96870a225ff124c5fa1ef90dbc138427
David Chinea <145079058+DavidChineaLive@users.noreply.github.com>
2025-04-07 01:00:14 -0500

    Remove api key accesss

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_lInJMFZJH_oceocrvvAt1de5hPBILGs",
+  apiKey: "API_KEY",
   authDomain: "sparrow-leathers.firebaseapp.com",
   projectId: "sparrow-leathers",
   storageBucket: "sparrow-leathers.firebasestorage.app",
-  messagingSenderId: "418413201076",
-  appId: "1:418413201076:web:8e6667e7bbede6cbf1b9e1",
-  measurementId: "G-DGH2KV74YS"
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_KEY",
+  measurementId: "MEASUREMENT_ID"
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
